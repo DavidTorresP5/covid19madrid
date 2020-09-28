cfa0b5c7ea5f5820337ab1a63276c3c1388b2f80
davinio <davidtorresp5@gmail.com[core]>
2020-09-28 13:57:31 +0200

    change dash.Dash() parameters

--- app.py
+++ app.py
@@ -8,11 +8,7 @@ import pandas as pd
 
 
 # App
-app = dash.Dash(
-    __name__,
-    external_stylesheets=[dbc.themes.COSMO],
-    meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
-)
+app = dash.Dash(__name__)
 
 server = app.server
 
